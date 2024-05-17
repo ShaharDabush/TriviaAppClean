b96b6557d937f05f549f26f98eab861bf2a3c852
dabus <dabus@DESKTOP-744J85B>
2024-05-17 18:29:04 +0300

    Styleeeeeeeeeee

--- Views/PlayerDetailsView.xaml.cs
+++ Views/PlayerDetailsView.xaml.cs
@@ -1,9 +1,12 @@
+using TriviaAppClean.ViewModels;
+
 namespace TriviaAppClean.Views;
 
 public partial class PlayerDetailsView : ContentPage
 {
-	public PlayerDetailsView()
+	public PlayerDetailsView(PlayerDetailsViewModel vm)
 	{
 		InitializeComponent();
-	}
+        this.BindingContext = vm;
+    }
 }
