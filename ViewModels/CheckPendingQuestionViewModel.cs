2b5ca91248af06e597bf8a1d208ac19d8107ed53
Yarden Golan <yarden.golan07@gmail.com>
2024-05-17 17:06:15 +0300

    .

--- ViewModels/CheckPendingQuestionViewModel.cs
+++ ViewModels/CheckPendingQuestionViewModel.cs
@@ -1,12 +1,80 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using TriviaAppClean.Models;
+using TriviaAppClean.Services;
 
 namespace TriviaAppClean.ViewModels
 {
     public class CheckPendingQuestionViewModel : ViewModelBase
     {
+        private TriviaWebAPIProxy _proxy;
+        private ObservableCollection<AmericanQuestion> questions;
+        public ObservableCollection<AmericanQuestion> Questions
+        {
+            get { return questions; }
+            set
+            {
+                questions = value;
+                OnPropertyChanged();
+            }
+        }
+        public CheckPendingQuestionViewModel()
+        {
+            _proxy = new TriviaWebAPIProxy();
+            GetQuestionsAsync();
+        }
+        private string query;
+        public string Query
+        {
+            get { return query; }
+            set { query = value; OnPropertyChanged(); }
+        }
+        private AmericanQuestion selectedQuestion;
+        public AmericanQuestion SelectedQuestion
+        {
+            get { return selectedQuestion; }
+            set { selectedQuestion = value; OnPropertyChanged(); }
+        }
+        public ICommand SingleSelectCommand => new Command(OnSingleSelectQuestion);
+        async void OnSingleSelectQuestion()
+        {
+            if (SelectedQuestion != null)
+            {
+                var navParam = new Dictionary<string, object>()
+                {
+                    { "selectedQuestion",SelectedQuestion }
+                };
+                await Shell.Current.GoToAsync($"PendingQuestionDetailsView", navParam);
+                SelectedQuestion = null;
+            }
+        }
+
+        public async void GetQuestionsAsync()
+        {
+            inServerCall = true;
+            List<AmericanQuestion> qs = await _proxy.GetAllQuestions();
+            qs = qs.Where(q => q.Status == 0).ToList();
+            Questions = new ObservableCollection<AmericanQuestion>(qs);
+            inServerCall = false;
+        }
+        
+        private bool inServerCall;
+        public bool InServerCall
+        {
+            get
+            {
+                return this.inServerCall;
+            }
+            set
+            {
+                this.inServerCall = value;
+                OnPropertyChanged();
+            }
+        }
     }
 }
